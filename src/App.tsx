7df88a78f5f8d1562cca0b4d662810ccfce664ca
Igor-Blazhko <i.blazhko@nv.dunice.net>
2026-01-28 15:00:54 +0300

    fix: Убрал автоудаление компонента

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { Module } from './modules'
 
 function App() {
 
-  const [o, seto] = useState(true);
-  useEffect(() => {
-    setTimeout(seto, 5000, false)
-  }, [])
-  return o ? (<Module />): null
+  return <Module />
 }
 
 export default App
